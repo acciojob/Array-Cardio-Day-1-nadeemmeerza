862bf5c1dda857b82483ad20e728a51d861f9571
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-30 15:02:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ export function sortByLastName() {
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
 
-export function reducedSum(data) {
+export function reducedSum() {
     // Return an object containing transports as key and its number of occurances 
 	//as the key's value
 	return data.reduce((accumulator, item)=>{
